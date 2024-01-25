ddf5eee7088cbcc2a57945da4ee86fb729a3b2f7
kory-lawal <122196271+kory-lawal@users.noreply.github.com>
2024-01-25 12:26:25 +0100

    Add files via upload

---
+++ wearther-app/script.js
@@ -0,0 +1,35 @@
+async function getWeather() {
+  const cityInput = document.getElementById("cityInput");
+  const weatherResult = document.getElementById("weatherResult");
+
+  const cityName = cityInput.value.trim();
+
+  if (cityName === "") {
+      alert("Please enter a city name.");
+      return;
+  }
+
+  try {
+      const apiKey = '030f5151a83286edfc3cf59716cd0698'; // Replace with your actual API key
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+      
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+          throw new Error('City not found. Please enter a valid city name.');
+      }
+
+      const data = await response.json();
+
+      const weatherInfo = `
+          <h2>${data.name}, ${data.sys.country}</h2>
+          <p>Temperature: ${data.main.temp} &#8451;</p>
+          <p>Weather: ${data.weather[0].description}</p>
+      `;
+
+      weatherResult.innerHTML = weatherInfo;
+  } catch (error) {
+      alert(error.message);
+      weatherResult.innerHTML = "";
+  }
+}
